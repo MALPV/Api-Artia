832ffed3bbf3513e77d77281ca356899da824b7f
Marcos Valenzuela <malpvdeveloper@gmail.com>
2025-06-30 19:09:23 -0400

    refactor: Se modifca mensaje de web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'status' => 'success',
-        'message' => 'API Artia funcionando correctamente'
+        'message' => 'Api-Artia funcionando correctamente 2025' 
     ]);
 });
